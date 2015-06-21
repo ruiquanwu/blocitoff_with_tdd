02bdda83aab91dfd901641d2abc0a3d41cd7e68e
ruiquan wu <ruiquanwu@gmail.com>
2015-06-21 20:03:07 +0000

    completed user authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params_user)
+    if @user.save
+      redirect_to root_url, :notice => "Signed up!"
+    else
+      render "new"
+    end
+  end
+  
+  private
+  
+  def params_user
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150621192858) do
+ActiveRecord::Schema.define(version: 20150621194255) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
